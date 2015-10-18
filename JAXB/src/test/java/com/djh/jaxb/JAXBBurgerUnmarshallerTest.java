1a15965c6309bfe496b70537a78ca8169524add6
daves125125 <daves125125@gmail.com>
2015-10-18 21:32:36 +0100

    initial commit

--- JAXB/src/test/java/com/djh/jaxb/JAXBBurgerUnmarshallerTest.java
+++ JAXB/src/test/java/com/djh/jaxb/JAXBBurgerUnmarshallerTest.java
@@ -1,7 +1,25 @@
 package com.djh.jaxb;
 
+import com.djh.xml.domain.Burger;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.io.InputStream;
+
 /**
  * @author David Hancock
  */
 public class JAXBBurgerUnmarshallerTest {
+
+    private JAXBBurgerUnmarshaller jaxbBurgerUnmarshaller = new JAXBBurgerUnmarshaller();
+
+    @Test
+    public void testUnmarshaller() {
+
+        InputStream inputStream = this.getClass().getClassLoader().getResourceAsStream("BurgerSample.xml");
+
+        Burger burger = jaxbBurgerUnmarshaller.unmarshalBurger(inputStream);
+        Assert.assertNotNull(burger);
+        Assert.assertEquals("Boar", burger.getMeatType());
+    }
 }
